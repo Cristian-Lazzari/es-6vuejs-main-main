d61c97cce2e715686bfadb013d30105c30c90fd7
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2023-10-06 15:33:52 +0200

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,151 @@
+<script >
+export default {
+  data() {
+    return {
+      generalmenu: 0,
+    }
+  },
+  methods: {
+    menuside() {
+      if (this.generalmenu) {
+        this.generalmenu = 0
+      } else {
+        this.generalmenu = 1
+      }
+      console.log(this.generalmenu)
+    },
+
+  }
+}
+</script>
+
+<template>
+  <div class="header">
+    <div class="logo"><img src="https://grace.energy/wp-content/uploads/2022/03/grace-logo.svg" alt=""></div>
+    <div class="nav">
+      <div class="full-lang"><a href="#">ENGLISH</a></div>
+      <div class="short-lang"><a href="#">EN</a></div>
+      <div class="buy"><a href="#">BUY</a></div>
+      <div class="user"><a href="#"><i class="bi bi-person"></i></a></div>
+      <div class="bag"><a href="#"><i class="bi bi-bag"></i></a></div>
+      <div class="menu-text"><a href="#">MENU</a></div>
+      <div class="menu" @click=" menuside(generalmenu)" :class="generalmenu ? 'menuopen' : 'menuclose'"><a href="#"><i class="bi bi-list"></i></a></div>
+    </div>
+  </div>
+  <div class="menusideopen" :class="generalmenu ? 'menusideopen' : 'menusideclose'">
+    <div class="menuopen-left"></div>
+    <div class="menuopen-right">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, qui aliquid! Molestiae necessitatibus nesciunt, quasi saepe ea, nulla itaque exercitationem adipisci aperiam neque cum impedit harum, suscipit sunt provident veritatis.</div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use "../assets/styles/general.scss";
+
+.header {
+  padding: 50px 50px;
+  background-color: #999B81;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+}
+
+.header a {
+  font-size: 17px;
+}
+.nav {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 25px;
+}
+
+.lang a:hover {
+  color: #FBED3F;
+}
+
+.buy a {
+  color: black;
+  background-color: #FBED3F;
+  padding: 5px 10px 0px 10px;
+}
+
+.user i {
+  font-size: 25px;
+  display: flex;
+  align-items: center;
+}
+
+.bag i {
+  font-size: 20px;
+  display: flex;
+  align-items: center;
+}
+
+.menu i {
+  font-size: 25px;
+  display: flex;
+  align-items: center;
+}
+
+.menuopen {
+  background-color: #FBED3F;
+  z-index: 1;
+}
+
+.menuclose {
+  
+}
+
+.menusideopen {
+  background-color: white;
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  right: 0;
+  display: flex;
+  // transition: display 1s linear ;
+  // transition: width 1s linear 1s ;
+  transition: all  linear 1s ;
+}
+.menusideclose {
+  transition: all  linear 1s ;
+  //display: none;
+  width: 0;
+  // transition: display 1s linear  ;
+  // transition: width 1s linear 1s ;
+}
+
+.menuopen-left{
+  width: 60%;
+  background-color: red;
+}
+.menuopen-right{
+  display: flex;
+  align-items: center;
+  width: 40%;
+}
+.short-lang{
+  display: none;
+}
+@media (max-width: 870px) {
+.menu-text{
+  display: none;
+}
+.nav{
+  gap: 15px;
+}
+.full-lang{
+  display: none;
+}
+.short-lang{
+  display: inline-block;
+}
+}
+@media (max-width: 530px) {
+  .header{
+    padding: 50px 10px;
+  }
+}
+</style>
---
+++ src/components/Pag5.vue
@@ -0,0 +1,147 @@
+<script>
+export default {
+  data() {
+    return {
+     sidebar1: 0,
+     sidebar2: 0,
+     sidebar3: 0,
+    }
+  },
+  methods: {
+    barside() {
+      if (this.sidebar1) {
+        this.sidebar1 = 0
+      } else {
+        this.sidebar1 = 1
+      }
+      console.log(this.sidebar1)
+    }
+}
+}
+</script>
+
+<template>
+  <div class="pag5">
+    <div class="container">
+        <div class="title-pag5"><h2>3 METABOLIC BOOSTERS</h2></div>
+        <div class="container-bar">
+            <div class="bar1">
+                <div class="top-bar1">
+                    <h3>AMINO ACIDS</h3>
+                    <span @click="generalbar(this.sidebar1)" :class="sidebar1 ? 'sidebar1open' :'sidebar1close'"  ><i class="bi bi-plus"></i></span>
+                </div>
+                <div class="bottom-bar1">
+                    <div  :class="sidebar1 ? 'sidecontent1open' :'sidecontent1close'">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi quibusdam quod debitis maxime. Tempore iusto dolorem odit eum. Natus labore ab blanditiis reprehenderit debitis voluptatum accusamus facere impedit dolor quod.
+                    </div> 
+                </div>
+            </div>
+            <div class="bar2">
+                <div class="top-bar2">
+                    <h3>AMINO ACIDS</h3>
+                    <span @click="generalbar(this.sidebar2)" :class="sidebar2 ? 'sidebar1open' :'sidebar1close'"  ><i class="bi bi-plus"></i></span>
+                </div>
+                <div class="bottom-bar2">
+                    <div  :class="sidebar2 ? 'sidecontent1open' :'sidecontent1close'">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi quibusdam quod debitis maxime. Tempore iusto dolorem odit eum. Natus labore ab blanditiis reprehenderit debitis voluptatum accusamus facere impedit dolor quod.
+                    </div> 
+                </div>
+            </div>
+            <div class="bar3">
+                <div class="top-bar3">
+                    <h3>AMINO ACIDS</h3>
+                    <span @click="generalbar(this.sidebar3)" :class="sidebar3 ? 'sidebar1open' :'sidebar1close'"  ><i class="bi bi-plus"></i></span>
+                </div>
+                <div class="bottom-bar3">
+                    <div  :class="sidebar1 ? 'sidecontent1open' :'sidecontent1close'">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi quibusdam quod debitis maxime. Tempore iusto dolorem odit eum. Natus labore ab blanditiis reprehenderit debitis voluptatum accusamus facere impedit dolor quod.
+                    </div> 
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use "../assets/styles/general.scss";
+.pag5{
+    background-color:#1a1a17 ;
+    padding-top: 100px;
+}
+.container{
+    padding: 0px 100px;
+}
+.title-pag5{
+    display: flex;
+    align-items: center;
+    padding-bottom: 100px;
+}
+.title-pag5 h2{
+    font-size: 70px;
+    color: #FFFFFF;
+}
+.bar1, .bar2, .bar3{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 30px;
+    color: #FFFFFF;
+    border-top: 1px solid white;
+    padding: 20px 0px;
+}
+.bar1 i, .bar2 i, .bar3 i{
+    font-size: 80px;
+}
+.bar2{
+    opacity: .5;
+}
+.bar3{
+    opacity: .3;
+    border-bottom: 1px solid white;
+    padding-bottom: 100px;
+}
+.sidebar1open{
+    transform: rotateZ(45deg);
+}
+.sidecontent1open {
+    padding: 3rem 0rem;
+    display: block;
+}
+.sidecontent1close{
+    display: none;
+}
+.top-bar1,.top-bar2,.top-bar3{
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.bottom-bar1{
+//display: none;
+}
+@media (max-width: 770px) {
+    .container{
+        padding: 0px 20px;
+    }
+    .title-pag5 h2{
+        font-size: 50px;
+    }
+    .bar1 i, .bar2 i, .bar3 i{
+    font-size: 60px;
+    .bar1 h3, .bar2 h3, .bar3 h3{
+        font-size: 30px;
+    }
+    }
+    .bar2{
+        flex-wrap: wrap;
+    }
+    
+
+
+    
+}
+    
+
+</style>
